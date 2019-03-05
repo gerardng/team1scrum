22a4eb79a081e82a30d0a72500b926b1c0ccff7e
Gerard Ng <gerardng@users.noreply.github.com>
2019-03-04 19:26:40 -0800

    Added Kuljit

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var options = ["Daniel", "Dylan", "Devan", "Gerard", "Gilbert", "Jacqueline", "Joe", "Judith", "Kelly", "Shengnan"];
+var options = ["Daniel", "Dylan", "Devan", "Gerard", "Gilbert", "Jacqueline", "Joe", "Judith", "Kelly", "Kuljit", "Shengnan"];
 
 var startAngle = 0;
 var arc = Math.PI / (options.length / 2);
